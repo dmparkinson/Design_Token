ddc1dff9469ec0b84b50691cf3d760a32b767d0e
Daniela Parkinson <55879943+dmparkinson@users.noreply.github.com>
2023-07-11 12:45:20 -0600

    First Configuration
    First Configuration Style

---
+++ build/ios-swift/StyleDictionary+Class.swift
@@ -0,0 +1,14 @@
+
+//
+// StyleDictionary+Class.swift
+//
+
+// Do not edit directly
+// Generated on Tue, 11 Jul 2023 18:26:21 GMT
+
+
+import UIKit
+
+public class StyleDictionaryClass {
+    public static let tokenSetOrder0 = global
+}
---
+++ build/ios-swift/StyleDictionary+Struct.swift
@@ -0,0 +1,14 @@
+
+//
+// StyleDictionary+Struct.swift
+//
+
+// Do not edit directly
+// Generated on Tue, 11 Jul 2023 18:26:21 GMT
+
+
+import UIKit
+
+internal struct StyleDictionaryStruct {
+    internal static let tokenSetOrder0 = global
+}
